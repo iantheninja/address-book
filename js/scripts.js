1ca756a127d9fe2ee63ed83343dba66ce0957266
iantheninja <munene.ian@live.com>
2020-06-01 12:35:27 +0300

    id's assigned to new contacts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
+// business logic for address book
+function AddressBook() {
+    this.contacts = [];
+    this.currentId = 0;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+    contact.id = this.assignId();
+    this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function () {
+    return this.currentId += 1;;
+}
+
+// business logic for contacts
 function Contact(firstName, lastName, phoneNumber) {
     this.firstName = firstName;
     this.lastName = lastName;
