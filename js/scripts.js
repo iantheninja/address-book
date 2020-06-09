4dca506ed461eda55ce2484a33ff891b6112fe2a
iantheninja <munene.ian@live.com>
2020-06-09 19:32:10 +0300

    equality operator bug removed, selected contact displayable in separate section

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// business logic for address book
+// Business logic for address book
 function AddressBook() {
     this.contacts = [];
     this.currentId = 0;
@@ -10,13 +10,13 @@ AddressBook.prototype.addContact = function(contact) {
 }
 
 AddressBook.prototype.assignId = function () {
-    return this.currentId += 1;;
+    return this.currentId += 1;
 }
 
 AddressBook.prototype.findContact = function(id) {
     for (let i = 0; i < this.contacts.length; i++) {
         if (this.contacts[i]) {      
-            if (this.contacts[i].id === id) {
+            if (this.contacts[i].id == id) {
                 return this.contacts[i];
             }
         }
@@ -27,7 +27,7 @@ AddressBook.prototype.findContact = function(id) {
 AddressBook.prototype.deleteContact = function(id) {
     for (let i = 0; i < this.contacts.length; i++) {
         if (this.contacts[i]) {      
-            if (this.contacts[i].id === id) {
+            if (this.contacts[i].id == id) {
                 delete this.contacts[i];
                 return true;
             }
@@ -36,7 +36,7 @@ AddressBook.prototype.deleteContact = function(id) {
     return false;
 }
 
-// business logic for contacts
+// Business logic for contacts
 function Contact(firstName, lastName, phoneNumber) {
     this.firstName = firstName;
     this.lastName = lastName;
@@ -46,3 +46,52 @@ function Contact(firstName, lastName, phoneNumber) {
 Contact.prototype.fullName = function() {
     return this.firstName + " " + this.lastName;
 }
+
+// User interface logic
+let addressBook = new AddressBook();
+
+function displayContactDetails(addressBookToDisplay) {
+    let contactsList = $("ul#contacts"); // query the dom once, multiple queries can slow down page
+    let htmlForContactInfo = "";
+    addressBookToDisplay.contacts.forEach(function(contact){
+        htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+    });
+    contactsList.html(htmlForContactInfo);
+}
+
+function showContact(contactId) {
+    const contact = addressBook.findContact(contactId);
+    $("#show-contact").show();
+
+    console.log(contact.firstName);
+
+    $(".first-name").html(contact.firstName);
+    $(".last-name").html(contact.lastName);
+    $(".phone-number").html(contact.phoneNumber);
+    let buttons = $("#buttons");
+    buttons.empty();
+    buttons.append("<button class='deleteButton' id=" + contact.id + ">Delete</button>");
+};
+
+// Enables list items added to page to be clickable (event bubbling/delegation)
+function attachContactListeners() {
+    $("ul#contacts").on("click", "li", function() {
+        console.log(this.id);
+        showContact(this.id);
+    });
+};
+
+/* Form submission block */
+$(document).ready(function() {
+    attachContactListeners();
+    $("form#new-contact").submit(function(event){
+        event.preventDefault();
+        const inputtedFirstName = $("input#new-first-name").val();
+        const inputtedLastName = $("input#new-last-name").val();
+        const inputtedPhoneNumber = $("input#new-phone-number").val();
+        let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+        addressBook.addContact(newContact);
+        displayContactDetails(addressBook);
+    })
+})
+
